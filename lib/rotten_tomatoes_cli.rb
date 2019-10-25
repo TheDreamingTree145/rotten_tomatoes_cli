ceb3c167c639ba7a99dc7c8529b4f2f54a5fcf80
TheDreamingTree145 <dandersonsoftware@outlook.com>
2019-10-25 15:48:53 -0400

    first commit

---
+++ lib/rotten_tomatoes_cli.rb
@@ -0,0 +1,6 @@
+require "rotten_tomatoes_cli/version"
+
+module RottenTomatoesCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rotten_tomatoes_cli/version.rb
@@ -0,0 +1,3 @@
+module RottenTomatoesCli
+  VERSION = "0.1.0"
+end
